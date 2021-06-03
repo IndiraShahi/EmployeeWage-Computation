21495d394aed4b6a37def2bf0fbb2191d9945b35
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-03 23:02:57 +0530

    [Indira] Added: Welcome message

---
+++ Employeewage-computation/Employeewage-computation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employeewage_computation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage");
+        }
+    }
+}
