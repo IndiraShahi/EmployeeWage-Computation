1ea79a7b30bdb5b29867416da90c482f11e977e8
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-04 00:07:51 +0530

    [Indira] Added: Monthly wage of employee

--- Employeewage-computation/Employeewage-computation/Program.cs
+++ Employeewage-computation/Employeewage-computation/Program.cs
@@ -7,28 +7,35 @@ namespace Employeewage_computation
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int Total_Working_Days = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wage");
             
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int TotalEmpWage = 0;
+            for (int day = 1; day <= Total_Working_Days; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                TotalEmpWage += empWage;
+                Console.WriteLine("Employee wage:" + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee wage:" + empWage);
+            Console.WriteLine("Total wage of employee :" + TotalEmpWage);
         }
     }
 }
