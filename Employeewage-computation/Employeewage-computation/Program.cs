16e64245fd2bc57e4f738201cce5265400d4a3c5
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-03 23:24:43 +0530

    [Indira] Added: Employee wage

--- Employeewage-computation/Employeewage-computation/Program.cs
+++ Employeewage-computation/Employeewage-computation/Program.cs
@@ -8,17 +8,21 @@ namespace Employeewage_computation
         {
             Console.WriteLine("Welcome to employee wage");
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
-            
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wage:" + empWage);
         }
     }
 }
