469e8116cb4b892f4c003b6ebe72d9e28b734d5e
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-11 18:52:27 +0530

    [Indira] ADDED: Save the total wage of each company

--- Employeewage-computation/Employeewage-computation/Program.cs
+++ Employeewage-computation/Employeewage-computation/Program.cs
@@ -4,44 +4,15 @@ namespace Employeewage_computation
 {
     class Program
     {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
         
-        public static int ComputeEmpWage(string company, int EMP_RATE_PER_HOUR, int MAX_Working_Days, int MAX_WORKING_HRS )
-        {
-            
-            int empHrs = 0;
-            int totalWorkingDays = 0;
-            int TotalEmpHrs = 0;
-            while(TotalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays <= MAX_Working_Days) 
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                TotalEmpHrs += empHrs;
-                Console.WriteLine("Working days: " + totalWorkingDays + " Employee working hours: " + empHrs);
-            }
-            int TotalEmpWage = TotalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine($"Total wage of {company} employee :" + TotalEmpWage);
-            return TotalEmpWage;
-        }
         static void Main(string[] args)
         {
-            ComputeEmpWage("Citigroup",400,20,160);
-            ComputeEmpWage("Goldmann Sachs",800, 22, 176);
-
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject goldmann = new EmpWageBuilderObject("Goldmann Sachs", 800, 22, 176);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            goldmann.computeEmpWage();
+            Console.WriteLine(goldmann.toString());
         }
     }
 }
