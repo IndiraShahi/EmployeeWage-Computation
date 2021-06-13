fab6254da0e04960c8ee5807666dbabda2ad82e9
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-13 21:53:57 +0530

    [Indira] ADDED: Ability to manage Employee Wage of multiple companies

--- Employeewage-computation/Employeewage-computation/Program.cs
+++ Employeewage-computation/Employeewage-computation/Program.cs
@@ -7,12 +7,16 @@ namespace Employeewage_computation
         
         static void Main(string[] args)
         {
-            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
-            EmpWageBuilderObject goldmann = new EmpWageBuilderObject("Goldmann Sachs", 800, 22, 176);
-            dMart.computeEmpWage();
-            Console.WriteLine(dMart.toString());
-            goldmann.computeEmpWage();
-            Console.WriteLine(goldmann.toString());
+           // EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+           // EmpWageBuilderObject goldmann = new EmpWageBuilderObject("Goldmann Sachs", 800, 22, 176);
+           // dMart.computeEmpWage();
+           // Console.WriteLine(dMart.toString());
+           // goldmann.computeEmpWage();
+           // Console.WriteLine(goldmann.toString());
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
         }
     }
 }
